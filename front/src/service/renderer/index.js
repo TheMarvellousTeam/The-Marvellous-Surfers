447b486da8932eecf6778720b798335e325e540a
Platane <me@platane.me>
2017-01-21 23:15:25 +0100

    :rocket:

--- front/src/service/renderer/index.js
+++ front/src/service/renderer/index.js
@@ -23,35 +23,6 @@ export const create = config => {
         scene.add( axisHelper )
     }
 
-    // LIGHTS
-    {
-        const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 )
-        hemiLight.color.setHSL( 0.6, 1, 0.6 )
-        hemiLight.groundColor.setHSL( 0.095, 1, 0.75 )
-        hemiLight.position.set( 0, 0, 500 )
-        hemiLight.name= 'hemilight'
-        scene.add( hemiLight )
-
-        //
-        const dirLight = new THREE.DirectionalLight( 0xffffff, 1 )
-        dirLight.color.setHSL( 0.1, 1, 0.95 )
-        dirLight.position.set( -1, 1.75, 1 )
-        dirLight.position.multiplyScalar( 5 )
-        dirLight.castShadow = true
-        dirLight.shadow.mapSize.width = 2048
-        dirLight.shadow.mapSize.height = 2048
-        dirLight.name= 'dirLight'
-        const d = 50
-        dirLight.shadow.camera.left = -d
-        dirLight.shadow.camera.right = d
-        dirLight.shadow.camera.top = d
-        dirLight.shadow.camera.bottom = -d
-        dirLight.shadow.camera.far = 3500
-        dirLight.shadow.bias = -0.0001
-
-        scene.add( dirLight )
-    }
-
     const setSize =  ( width, height ) => {
 
         camera.aspect = width / height
