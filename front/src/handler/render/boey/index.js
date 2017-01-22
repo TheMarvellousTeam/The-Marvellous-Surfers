6173be791017e6505003af0231e815e74c63d8aa
p4ddl3 <simon.daget@gmail.com>
2017-01-22 17:00:54 +0100

    ajout bouees

--- front/src/handler/render/boey/index.js
+++ front/src/handler/render/boey/index.js
@@ -11,7 +11,8 @@ const createSprite = createSpriteFactory({
 
 export const create = ( state, { renderer, bus } ) => {
 
-    ;[ 75, -75 ].map( x => {
+    /*
+    ;[ 100, -100 ].map( x => {
 
         const l = 1000
         const geo = new THREE.PlaneBufferGeometry( 30, l )
@@ -29,5 +30,70 @@ export const create = ( state, { renderer, bus } ) => {
 
         renderer.scene.add( sprite )
     })
+    */
+    const object_by_id = {}
+
+
+    const getSprite = id => {
+
+        if ( object_by_id[ id ] )
+            return object_by_id[ id ]
+
+        object_by_id[ id ] = createSprite()
+        object_by_id[ id ].name = 'boey'
+
+        return object_by_id[ id ]
+    }
+
+    let boeys = [];
+
+    ;[-1,1].forEach(x => {
+    for(let i = 0; i < 100; ++i) {
+    	
+	let boey = {
+		position : {
+			x : 100*x,
+			y : 30  *  i
+		},
+		id:(Math.random().toString(32).slice(2))
+	}
+	boeys.push(boey);
+    
+    }
+    });
+    const update = () => {
+	/*
+        Object.keys( object_by_id )
+            .filter( id => !waves.some( wave => id == wave.id ) )
+            .forEach( id => {
+
+                object_by_id[ id ].parent && object_by_id[ id ].parent.remove( object_by_id[ id ] )
+                object_by_id[ id ].destroy()
+                delete object_by_id[ id ]
+            })
+	*/
+
+        boeys.forEach( boey => {
+
+            const sprite = getSprite( boey.id )
+
+            if( !sprite.parent )
+                renderer.scene.add( sprite )
+
+            // update
+            {
+                // position
+                sprite.position.x = boey.position.x
+                sprite.position.y = boey.position.y - state.cammera_offset_y
+
+
+                // animation
+                sprite.setState( 0 )
+
+            }
+
+        })
+    }
+    bus.on('loop', () => update())
 
 }
