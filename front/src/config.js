313c2ef9f40696ba0d4881891c848bfaee32eceb
Platane <me@platane.me>
2017-01-22 01:35:58 +0100

    :rocket:

--- front/src/config.js
+++ front/src/config.js
@@ -1,4 +1,4 @@
 export const com = {
-    port            : 8080 || process.env.COM_PORT || 8080,
-    hostname        : 'localhost' || process.env.COM_HOST || 'localhost',
+    port            : process.env.COM_PORT || 8080,
+    hostname        : process.env.COM_HOST || 'localhost',
 }
