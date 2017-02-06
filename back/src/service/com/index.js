aa3f58937e68d3bc3c93fd9d308538a5216274a0
Platane <me@platane.me>
2017-02-07 00:27:32 +0100

    :rocket:

--- back/src/service/com/index.js
+++ back/src/service/com/index.js
@@ -1,18 +1,28 @@
 import engineIo         from 'engine.io'
 import {EventEmitter}   from 'events'
-
+import {createServer}   from 'http'
+import express          from 'express'
 
 export const create = config => {
 
     const ee     = new EventEmitter
     ee._emit = ee.emit
 
-    const server = engineIo.listen( config.port, err => console.log( err ? err : `listening to port ${config.port}` ) )
+    const app       = express()
+    const http      = createServer(app)
+
+    const live      = engineIo.attach(http)
+
+    http.listen( config.port, err => console.log( err ? err : `listening to port ${config.port}` ) )
+
+    app.use(express.static('dist'))
+
 
     ee.emit = ( socketId, type, payload={}, meta={} ) =>
-        server.clients[ socketId ] && server.clients[ socketId ].send( JSON.stringify({ type, payload, meta }) )
+        live.clients[ socketId ] && live.clients[ socketId ].send( JSON.stringify({ type, payload, meta }) )
 
-    server
+
+    live
         .on('connection', socket => {
 
             socket
--- front/src/config.js
+++ front/src/config.js
@@ -1,4 +1,4 @@
 export const com = {
-    port            : process.env.COM_PORT || 8080,
-    hostname        : process.env.COM_HOST || 'localhost',
+    port            : 80 || process.env.COM_PORT || 8080,
+    hostname        : location.host || process.env.COM_HOST || 'localhost',
 }
